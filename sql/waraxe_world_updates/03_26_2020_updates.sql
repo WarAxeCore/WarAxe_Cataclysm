411df55d43b8634a8ffb591ca140e73ba4d2ea56
Razfury <wolcronic@hotmail.com>
2023-03-31 12:19:58 -0400

    Fix SQL file.

--- sql/waraxe_world_updates/03_26_2020_updates.sql
+++ sql/waraxe_world_updates/03_26_2020_updates.sql
@@ -1,7 +1,7 @@
 -- Fix incorrect Control Demon spell
-UPDATE `npc_trainer` SET `spell` = '93375' WHERE `npc_trainer`.`spell` =80388;
+UPDATE `npc_trainer` SET `spell` = '93375' WHERE `npc_trainer`.`spell` = 80388;
 
---fix currency for cata
-update skyfire_world.quest_template set rewcurrencyid1 = 395, rewcurrencycount1 = 140 where entry = 28907;
-update skyfire_world.quest_template set rewcurrencyid1 = 395, rewcurrencycount1 = 70 where entry = 28906;
-update skyfire_world.quest_template set rewcurrencyid1 = 396, rewcurrencycount1 = 70 where entry = 28905;
+-- fix currency for cata
+update skyfire_world.quest_template set RewCurrencyId1 = 395, RewCurrencyCount1 = 140 where entry = 28907;
+update skyfire_world.quest_template set RewCurrencyId1 = 395, RewCurrencyCount1 = 70 where entry = 28906;
+update skyfire_world.quest_template set RewCurrencyId1 = 396, RewCurrencyCount1 = 70 where entry = 28905;
